6a121cedd382b13f8f3e2908c8891c1462c57d68
Shubham Patra <babaijobmail1218@gmail.com>
2021-07-12 02:54:45 +0530

    hubu

--- RahulShettySeleniumTraining/src/practictice/demo/Demo.java
+++ RahulShettySeleniumTraining/src/practictice/demo/Demo.java
@@ -2,7 +2,7 @@ package practictice.demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Demo {
 
@@ -11,8 +11,10 @@ public class Demo {
 		//System.setProperty("webdriver.chrome.driver", "D:\\Selenium Practices\\Required Softwares\\Browser Drivers\\Chrome Drivers\\91.0.4472.101\\chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		WebDriverWait wait = new WebDriverWait(driver, 15);
+		//WebDriverWait wait = new WebDriverWait(driver, 15);
 		driver.get("www.google.co.in");
+		
+		System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/src/drivers/msedgedriver.exe");
 	}
 
 }
