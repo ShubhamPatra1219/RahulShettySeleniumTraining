ca46b965ba7193fc4b597e4144e05b885816b6db
Shubham Patra <babaijobmail1218@gmail.com>
2021-07-12 02:46:32 +0530

    Rahul Shetty Learning Started.

---
+++ RahulShettySeleniumTraining/src/practictice/demo/Demo.java
@@ -0,0 +1,18 @@
+package practictice.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "D:\\Selenium Practices\\Required Softwares\\Browser Drivers\\Chrome Drivers\\91.0.4472.101\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		driver.get("www.google.co.in");
+	}
+
+}
